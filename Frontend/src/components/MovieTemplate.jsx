57554045ede7d809a980fd40679802f85f4bd105
Prajwal <prajwaldongare112@gmail.com>
2026-07-19 14:28:08 +0530

    fix: broken image

--- Frontend/src/components/MovieTemplate.jsx
+++ Frontend/src/components/MovieTemplate.jsx
@@ -5,7 +5,7 @@ const MovieTemplate = ({ movieName, summary, poster, genres = [] }) => {
     return (
         <>
             <div tabIndex={0} className="main m-4 w-60 relative group rounded-2xl overflow-hidden cursor-pointer shadow-lg outline-none">
-                <img src={poster || '../../public/templateImage.avif'} alt="movieImage" className='w-full h-90 object-cover transition-transform duration-300 group-hover:scale-105 group-focus:scale-105' />
+                <img src={poster || 'templateImage.avif'} alt="movieImage" className='w-full h-90 object-cover transition-transform duration-300 group-hover:scale-105 group-focus:scale-105' />
                 <div className="absolute inset-0 bg-linear-to-t from-gray-900 via-gray-900/90 to-transparent opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-5">
                     <div className="name mt-2 text-2xl font-bold text-white">{movieName}</div>
 
